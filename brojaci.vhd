6c3c0ec525a82e3d04a0e84ae9f1dd4f80eb094f
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2022-10-13 22:41:11 +0200

    Add files via upload

---
+++ brojaci.vhd
@@ -0,0 +1,26 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity samirovi_brojaci is
+port {
+iCLK : in std_logic;
+iRESET : std_logic;
+iCOUNT : std_logic;
+iINC : std_logic;
+oCNT1 : std_logic;
+oCNT2 : std_logic;
+oCMP : std_logic;
+end samirovi_brojaci;
+
+
+architecture Behavioral of samirovi_brojaci is 
+ 
+
+begin
+
+
+end Behavioral;
+
